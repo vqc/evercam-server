6c458fccd730d36d91c71ee0c0f26af55566ce00
Shankar Dhanasekaran <shankar@opendrops.com>
2015-10-06 14:40:01 +0530

    Handle invalid time inputs in scheduler

--- lib/evercam_media/schedule.ex
+++ lib/evercam_media/schedule.ex
@@ -9,40 +9,48 @@ defmodule EvercamMedia.Schedule do
   end
 
   defp iterate([head|tail], timezone) do
-    [from, to] = String.split head, "-"
-    [from_hour, from_minute] = String.split from, ":"
-    [to_hour, to_minute] = String.split to, ":"
+    # We expect the head to be in the format "HH:MM-HH:MM"
+    head_pattern = ~r/^\d{2}:\d{2}-\d{2}:\d{2}$/
+    case Regex.match? head_pattern, head do
+      true ->
+        [from, to] = String.split head, "-"
+        [from_hour, from_minute] = String.split from, ":"
+        [to_hour, to_minute] = String.split to, ":"
 
-    from_date = to_date(from_hour, from_minute, timezone)
-    to_date = to_date(to_hour, to_minute, timezone)
-    now = Timex.Date.now
+        from_date = to_date(from_hour, from_minute, timezone)
+        to_date = to_date(to_hour, to_minute, timezone)
+        now = Timex.Date.now
 
-    date_format = "{ISOz}"
-    {_, now_str} = Timex.DateFormat.format(now, date_format)
-    {_, from_str} = Timex.DateFormat.format(from_date, date_format)
-    {_, to_str} = Timex.DateFormat.format(to_date, date_format)
+        date_format = "{ISOz}"
+        {_, now_str} = Timex.DateFormat.format(now, date_format)
+        {_, from_str} = Timex.DateFormat.format(from_date, date_format)
+        {_, to_str} = Timex.DateFormat.format(to_date, date_format)
 
-    from_date_now = Timex.Date.compare(from_date, now)
-    now_to_date = Timex.Date.compare(now, to_date)
+        from_date_now = Timex.Date.compare(from_date, now)
+        now_to_date = Timex.Date.compare(now, to_date)
 
-    case {from_date_now, now_to_date} do
-      {-1, -1} ->
-        true
-      {0, -1} ->
-        true
-      {-1, 0} ->
-        true
+        case {from_date_now, now_to_date} do
+          {-1, -1} ->
+            {:ok, true}
+          {0, -1} ->
+            {:ok, true}
+          {-1, 0} ->
+            {:ok, true}
+          _ ->
+            iterate(tail, timezone)
+        end
       _ ->
-        iterate(tail, timezone)
+        {:error, "Scheduler got an invalid time format: #{inspect(head)}. Expecting Time in the format HH:MM-HH:MM"}
     end
+
   end
 
   defp iterate(nil, timezone) do
-    false
+    {:ok, false}
   end
 
   defp iterate([], timezone) do
-    false
+    {:ok, false}
   end
 
   defp to_date(hours, minutes, nil) do
