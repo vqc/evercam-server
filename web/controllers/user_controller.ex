022ec1aa4192af77c8eb6ba59bad1963865545ac
Ile Eftimov <ileeftimov@gmail.com>
2015-10-13 22:59:22 +0000

    introduce RegistrationFactory

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,34 +4,22 @@ defmodule EvercamMedia.UserController do
   def create(conn, %{ "user" => user_params }) do
     user_changeset = User.changeset(%User{}, user_params)
 
-    if user_changeset.valid? do
-      case Repo.insert(user_changeset) do
-        { :ok, user } ->
-          {:ok, exp_date } = Calendar.DateTime.now!("UTC") |> Calendar.DateTime.advance(3600) 
-          {:ok, expiry_date } = Calendar.DateTime.to_erl(exp_date) |> Ecto.DateTime.cast
-          token = Ecto.Model.build(user, :access_tokens, is_revoked: false, request: UUID.uuid4(:hex), expires_at: expiry_date )
-
-          Repo.insert(token) |> handle_token_insert(user, conn)
-        { :error, changeset } ->
-          conn |> handle_error(:conflict, changeset)
-      end
-    else
-      conn |> handle_error(:bad_request, user_changeset)
+    case EvercamMedia.RegistrationFactory.create(user_changeset) do
+      { :invalid_user, changeset } ->
+        handle_error(conn, :bad_request, changeset)
+      { :duplicate_user,  changeset } ->
+        handle_error(conn, :conflict, changeset)
+      { :invalid_token, changeset } ->
+        handle_error(conn, :unprocessable_entity, changeset)
+      { :success, user, token } ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("access-control-allow-origin", "*")
+        |> render("user.json", %{ user: user, token: token })
     end
   end
 
-  def update(conn, %{ "user" => user_params }) do
-  end
-
-  defp handle_token_insert({:ok, token}, user, conn) do
-    conn
-    |> put_status(:created)
-    |> put_resp_header("access-control-allow-origin", "*")
-    |> render("user.json", %{ user: user, token: token })
-  end
-
-  defp handle_token_insert({:error, changeset}, changeset, conn) do
-    conn |> handle_error(:unprocessable_entity, changeset)
+  def update(conn, %{ "token" => token, "user" => user_params }) do
   end
 
   defp handle_error(conn, status, changeset) do
---
+++ web/models/registration_factory.ex
@@ -0,0 +1,23 @@
+defmodule EvercamMedia.RegistrationFactory do
+  alias EvercamMedia.Repo
+
+  def create(user_changeset) do
+    if user_changeset.valid? do
+      case Repo.insert(user_changeset) do
+        { :ok, user } ->
+          {:ok, exp_date } = Calendar.DateTime.now!("UTC") |> Calendar.DateTime.advance(3600) 
+          {:ok, expiry_date } = Calendar.DateTime.to_erl(exp_date) |> Ecto.DateTime.cast
+          token = Ecto.Model.build(user, :access_tokens, is_revoked: false, request: UUID.uuid4(:hex), expires_at: expiry_date )
+
+          case Repo.insert(token) do
+            { :ok, token } -> { :success, user, token }
+            { :error, changeset } -> { :invalid_token, changeset }
+          end
+        { :error, changeset } -> { :duplicate_user, changeset }
+      end
+    else
+      { :invalid_user, user_changeset }
+    end
+  end
+end
+
