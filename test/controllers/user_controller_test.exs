8e4b22cbd21a8e9b7a3f7087531d359d6ba71553
Ile Eftimov <ileeftimov@gmail.com>
2015-10-13 17:42:31 +0000

    handle various errors that can happen on POST /v1/users

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule EvercamMedia.UserControllerTest do
   use EvercamMedia.ConnCase
 
-  test "POST /v1/users" do
+  test "POST /v1/users - creates the user and returns the json" do
     {:ok, country } = EvercamMedia.Repo.insert(%Country{ name: "Whatever", iso3166_a2: "WHTEVR" })
     params = %{ "user" => %{ firstname: "John" , lastname: "Doe", 
         email: "johndoe@example.com", username: "johnd", country_id: country.id, password: "some_password" } }
@@ -15,5 +15,44 @@ defmodule EvercamMedia.UserControllerTest do
     assert response["user"]["country_id"] == country.id
     refute response["user"]["id"] == ""
     assert response["user"]["token"] == AccessToken.active_token_for(response["user"]["id"]).request
+  end
+
+  test "POST /v1/users - returns a conflict error for a duplicate user name" do
+    {:ok, country } = EvercamMedia.Repo.insert(%Country{ name: "Whatever", iso3166_a2: "WHTEVR" })
+    {:ok, _user } = EvercamMedia.Repo.insert(%User{ firstname: "John", lastname: "Doe", 
+        email: "johndoe@example.com", username: "johnd", country_id: country.id, password: "some_password"})
+
+    params = %{ "user" => %{ firstname: "John" , lastname: "Dade", 
+        email: "john@dade.com", username: "johnd", country_id: country.id, password: "some_password" } }
+
+    conn = conn()
+      |> post("/v1/users", params)
+
+    response = json_response(conn, 409)
+    assert response["errors"]["username"], ["has already been taken"]
+  end
+
+  test "POST /v1/users - returns a conflict error for a duplicate email address" do
+    {:ok, country } = EvercamMedia.Repo.insert(%Country{ name: "Whatever", iso3166_a2: "WHTEVR" })
+    {:ok, _user } = EvercamMedia.Repo.insert(%User{ firstname: "John", lastname: "Doe", 
+        email: "johnd@example.com", username: "johnd", country_id: country.id, password: "some_password"})
+
+    params = %{ "user" => %{ firstname: "John" , lastname: "Dade", 
+        email: "johnd@example.com", username: "johndade", country_id: country.id, password: "some_password" } }
+
+    conn = conn()
+      |> post("/v1/users", params)
+
+    response = json_response(conn, 409)
+    assert response["errors"]["email"], ["has already been taken"]
+  end
+
+  test "POST /v1/users - returns a HTTP 400 for blank params" do
+    params = %{ "user" => %{ firstname: "" , lastname: "", email: "", username: "", country_id: "", password: "" } }
+
+    conn = conn()
+      |> post("/v1/users", params)
+
+    response = json_response(conn, 400)
   end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,27 +4,39 @@ defmodule EvercamMedia.UserController do
   def create(conn, %{ "user" => user_params }) do
     user_changeset = User.changeset(%User{}, user_params)
 
-    user = Repo.insert!(user_changeset)
+    if user_changeset.valid? do
+      case Repo.insert(user_changeset) do
+        { :ok, user } ->
+          {:ok, exp_date } = Calendar.DateTime.now!("UTC") |> Calendar.DateTime.advance(3600) 
+          {:ok, expiry_date } = Calendar.DateTime.to_erl(exp_date) |> Ecto.DateTime.cast
+          token = Ecto.Model.build(user, :access_tokens, is_revoked: false, request: UUID.uuid4(:hex), expires_at: expiry_date )
 
-    {:ok, exp_date } = Calendar.DateTime.now!("UTC") |> Calendar.DateTime.advance(3600) 
-    {:ok, expiry_date } = Calendar.DateTime.to_erl(exp_date) |> Ecto.DateTime.cast
-    token = Ecto.Model.build(user, :access_tokens, is_revoked: false, request: UUID.uuid4(:hex), expires_at: expiry_date )
-
-    case Repo.insert(token) do
-      {:ok, token } ->
-        conn
-        |> put_status(:created)
-        |> put_resp_header("access-control-allow-origin", "*")
-        |> render("user.json", %{ user: user, token: token })
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> put_resp_header("access-control-allow-origin", "*")
-        |> render(EvercamMedia.ChangesetView, "error.json", changeset: changeset)
+          case Repo.insert(token) do
+            {:ok, token } ->
+              conn
+              |> put_status(:created)
+              |> put_resp_header("access-control-allow-origin", "*")
+              |> render("user.json", %{ user: user, token: token })
+            {:error, changeset} ->
+              conn
+              |> put_status(:unprocessable_entity)
+              |> put_resp_header("access-control-allow-origin", "*")
+              |> render(EvercamMedia.ChangesetView, "error.json", changeset: changeset)
+          end
+        { :error, changeset } ->
+          conn
+          |> put_status(:conflict)
+          |> put_resp_header("access-control-allow-origin", "*")
+          |> render(EvercamMedia.ChangesetView, "error.json", changeset: changeset)
+      end
+    else
+      conn
+      |> put_status(:bad_request)
+      |> put_resp_header("access-control-allow-origin", "*")
+      |> render(EvercamMedia.ChangesetView, "error.json", changeset: user_changeset)
     end
   end
 
   def update(conn, %{ "token" => token, "user" => user_params }) do
   end
-
 end
