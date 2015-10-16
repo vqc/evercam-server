82b7cf23a0c951fcf119f985b6b45ba0ecf3d187
Ile Eftimov <ileeftimov@gmail.com>
2015-10-16 08:18:59 +0000

    fix constraint name on Country

--- web/models/country.ex
+++ web/models/country.ex
@@ -14,6 +14,6 @@ defmodule Country do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-    |> unique_constraint(:iso_3166_a2)
+    |> unique_constraint(:iso_3166_a2, [name: "ux_countries_iso3166_a2"])
   end
 end
