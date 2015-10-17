4ba488301c58535c85af22c5efb9556a1cb636a5
Ile Eftimov <ileeftimov@gmail.com>
2015-10-17 16:33:42 +0000

    reenable creation of camera records for controller tests

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -39,8 +39,8 @@ defmodule EvercamMedia.ConnCase do
     # Wrap this case in a transaction
     Ecto.Adapters.SQL.begin_test_transaction(EvercamMedia.Repo)
 
-    #camera = %Camera{config: %{"auth" => %{"basic" => %{"password" => "mehcam", "username" => "admin"}}, "external_host" => "149.13.244.32", "external_http_port" => 8100, "external_rtsp_port" => 9100, "internal_host" => "", "internal_http_port" => "", "internal_rtsp_port" => "", "snapshots" => %{"jpg" => "/Streaming/Channels/1/picture"}}, exid: "mobile-mast-test", id: 5, is_online: true, is_public: false, name: "Test Mobile Mast", owner_id: 2}
-    #EvercamMedia.Repo.insert(camera)
+    camera = %Camera{config: %{"auth" => %{"basic" => %{"password" => "mehcam", "username" => "admin"}}, "external_host" => "149.13.244.32", "external_http_port" => 8100, "external_rtsp_port" => 9100, "internal_host" => "", "internal_http_port" => "", "internal_rtsp_port" => "", "snapshots" => %{"jpg" => "/Streaming/Channels/1/picture"}}, exid: "mobile-mast-test", id: 5, is_online: true, is_public: false, name: "Test Mobile Mast", owner_id: 2}
+    EvercamMedia.Repo.insert(camera)
 
     # Roll it back once we are done
     on_exit fn ->
