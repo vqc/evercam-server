d6bff4877a2bd09ab2dbfdc27d8e5489b550d632
Shankar Dhanasekaran <shankar@opendrops.com>
2015-11-02 17:57:53 +0530

    Copy useful functions from SnapshotFetch into Evercam.Util

--- lib/evercam_media/util.ex
+++ lib/evercam_media/util.ex
@@ -1,4 +1,6 @@
 defmodule EvercamMedia.Util do
+  require Logger
+
   def decode_request_token(token) do
     {_, encrypted_message} = Base.url_decode64(token)
     message = :crypto.block_decrypt(
@@ -8,5 +10,34 @@ defmodule EvercamMedia.Util do
       encrypted_message
     )
     String.split(message, "|")
+  end
+
+  def broadcast_snapshot(camera_id, image) do
+    EvercamMedia.Endpoint.broadcast(
+      "cameras:#{camera_id}",
+      "snapshot-taken",
+      %{image: Base.encode64(image)}
+    )
+  end
+
+  def s3_file_url(file_name) do
+    configure_erlcloud
+    "/" <> name = file_name
+    name   = String.to_char_list(name)
+    bucket = System.get_env("AWS_BUCKET") |> String.to_char_list
+    {expires, host, uri} = :erlcloud_s3.make_link(100000000, bucket, name)
+    "#{to_string(host)}#{to_string(uri)}"
+  end
+
+  def error_handler(error) do
+    Logger.error inspect(error)
+    Logger.error Exception.format_stacktrace System.stacktrace
+  end
+
+  defp configure_erlcloud do
+    :erlcloud_s3.configure(
+      to_char_list(System.get_env["AWS_ACCESS_KEY"]),
+      to_char_list(System.get_env["AWS_SECRET_KEY"])
+    )
   end
 end
