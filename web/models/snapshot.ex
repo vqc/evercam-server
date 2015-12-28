485f2171fa220dd3e3608d2c7986b2e31ab30f12
Miloš Mošić <mosic.milos@gmail.com>
2015-12-29 00:30:23 +0100

    Remove unused for_camera/2 method

--- web/models/snapshot.ex
+++ web/models/snapshot.ex
@@ -15,12 +15,4 @@ defmodule Snapshot do
     where: snap.camera_id == ^camera_id,
     select: snap
   end
-
-  def for_camera(camera_id,timestamp) do
-    from snap in Snapshot,
-    where: snap.camera_id == ^camera_id and snap.created_at == ^timestamp,
-    select: snap,
-    order_by: [desc: snap.created_at],
-    limit: 1
-  end
 end
