495ff37261bdcc218494de409c81fb22886ef1a6
Miloš Mošić <mosic.milos@gmail.com>
2015-12-29 01:03:52 +0100

    Add expired query

--- web/models/snapshot.ex
+++ web/models/snapshot.ex
@@ -15,4 +15,16 @@ defmodule Snapshot do
       where: snap.camera_id == ^camera_id,
       select: snap
   end
+
+  def expired_by_cloud_recording(cloud_recording) do
+    seconds_to_expired_day = cloud_recording.storage_duration * 24 * 60 * 60 * -1
+    expired_day = Calendar.DateTime.now_utc |> Calendar.DateTime.advance!(seconds_to_expired_day)
+    begin_timestamp = expired_day |> Calendar.Strftime.strftime! "%Y%m%d"
+    end_timestamp = expired_day |> Calendar.Date.next_day! |> Calendar.Strftime.strftime! "%Y%m%d"
+
+    from snap in Snapshot,
+      where: snap.snapshot_id > "#{cloud_recording.camera_id}_#{begin_timestamp}",
+      where: snap.snapshot_id < "#{cloud_recording.camera_id}_#{end_timestamp}",
+      where: snap.notes == "Evercam Proxy"
+  end
 end
