090841f4197f4fc86d112ef8ee198a7a2b919be1
Miloš Mošić <mosic.milos@gmail.com>
2015-12-29 01:42:21 +0100

    Pad expired snapshots by a day

--- web/models/snapshot.ex
+++ web/models/snapshot.ex
@@ -29,7 +29,7 @@ defmodule Snapshot do
   end
 
   defp expired(camera_id, storage_duration) do
-    seconds_to_expired_day = storage_duration * 24 * 60 * 60 * -1
+    seconds_to_expired_day = (storage_duration + 1) * (24 * 60 * 60) * (-1)
     expired_day = DateTime.now_utc |> DateTime.advance!(seconds_to_expired_day)
     begin_timestamp = expired_day |> Strftime.strftime! "%Y%m%d"
     end_timestamp = expired_day |> Date.next_day! |> Strftime.strftime! "%Y%m%d"
