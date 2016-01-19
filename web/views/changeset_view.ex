9f7c6d99af7b7215a3b40093f3a4695e33dc16a8
jgarciasacristan <jose.garciasacristan@gmail.com>
2016-01-19 22:14:18 +0000

    fix UserController tests (except 1)

--- web/views/changeset_view.ex
+++ web/views/changeset_view.ex
@@ -2,7 +2,7 @@ defmodule EvercamMedia.ChangesetView do
   use EvercamMedia.Web, :view
 
   def render("error.json", %{changeset: changeset}) do
-    %{errors: changeset}
+    %{errors: Enum.into(changeset.errors, %{})}
   end
 
 end
