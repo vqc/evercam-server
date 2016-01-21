ad3690617bace162dfa883b8ccc26902d6d9d917
Miloš Mošić <mosic.milos@gmail.com>
2016-01-21 16:56:26 +0100

    Invalidate cache on thumbnail update

--- lib/evercam_media/snapshot/db_handler.ex
+++ lib/evercam_media/snapshot/db_handler.ex
@@ -162,6 +162,7 @@ defmodule EvercamMedia.Snapshot.DBHandler do
 
     if update_thumbnail? && stale_thumbnail?(camera.thumbnail_url, timestamp) do
       update_thumbnail(camera, timestamp)
+      invalidate_camera_cache(camera)
     end
 
     camera
