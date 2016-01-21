bbab9abf1dbd702a92811020dd53e51a909a2e0c
Miloš Mošić <mosic.milos@gmail.com>
2016-01-21 14:31:36 +0100

    Formatting

--- web/controllers/onvif_access_plug.ex
+++ web/controllers/onvif_access_plug.ex
@@ -5,15 +5,17 @@ defmodule EvercamMedia.ONVIFAccessPlug do
   end
 
   def call(conn, _) do
-    access_info =  case conn.query_params do
-                     %{"auth" => _auth, "url" => _url} -> conn.query_params
-                     %{"id" => id} -> Camera.get_camera_info id
-                     _ -> Camera.get_camera_info conn.params["id"]
-                   end
+    access_info =
+      case conn.query_params do
+        %{"auth" => _auth, "url" => _url} -> conn.query_params
+        %{"id" => id} -> Camera.get_camera_info(id)
+        _ -> Camera.get_camera_info(conn.params["id"])
+      end
+
     parameters =
       conn.query_params
       |> Enum.filter(fn({key, _value}) -> key != "id" and key != "url" and key != "auth" end)
-      |> Enum.reduce("", fn({key,value}, acc) -> "#{acc}<#{key}>#{value}</#{key}>" end)
+      |> Enum.reduce("", fn({key, value}, acc) -> "#{acc}<#{key}>#{value}</#{key}>" end)
 
     conn
     |> assign(:onvif_parameters, parameters)
