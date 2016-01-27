7754c114b6e152a6deafeaaa1ce68e4f975551d1
Miloš Mošić <mosic.milos@gmail.com>
2016-01-27 21:16:03 +0100

    Implement building ranges of expired snapshots

--- web/models/snapshot.ex
+++ web/models/snapshot.ex
@@ -1,5 +1,6 @@
 defmodule Snapshot do
   use EvercamMedia.Web, :model
+  import Ecto.Query
   alias Calendar.Date
   alias Calendar.DateTime
   alias Calendar.Strftime
@@ -19,24 +20,62 @@ defmodule Snapshot do
     field :created_at, Ecto.DateTime, default: Ecto.DateTime.utc
   end
 
+  def delete_by_range(camera_id, [start, finish]) do
+    Snapshot
+    |> where([snap], snap.snapshot_id > ^"#{camera_id}_#{start}")
+    |> where([snap], snap.snapshot_id < ^"#{camera_id}_#{finish}")
+    |> SnapshotRepo.delete_all
+  end
+
   def expired(cloud_recording) do
     expired(cloud_recording.camera_id, cloud_recording.storage_duration)
   end
 
-  defp expired(_camera_id, -1), do: []
-  defp expired(camera_id, storage_duration) do
+  def expired(_camera_id, -1), do: [[], []]
+  def expired(camera_id, storage_duration) do
     seconds_to_expired_day = (storage_duration + 1) * (24 * 60 * 60) * (-1)
-    expired_day = DateTime.now_utc |> DateTime.advance!(seconds_to_expired_day)
-    begin_timestamp = expired_day |> Strftime.strftime!("%Y%m%d")
-    end_timestamp = expired_day |> Date.next_day! |> Strftime.strftime!("%Y%m%d")
+    expired_day =
+      DateTime.now_utc
+      |> DateTime.advance!(seconds_to_expired_day)
+    begin_timestamp =
+      expired_day
+      |> Strftime.strftime!("%Y%m%d")
+      |> String.ljust(14, ?0)
+    end_timestamp =
+      expired_day
+      |> Date.next_day!
+      |> Strftime.strftime!("%Y%m%d")
+      |> String.ljust(14, ?0)
+    any_snapshot =
+      Snapshot
+      |> where([snap], snap.snapshot_id > ^"#{camera_id}_#{begin_timestamp}")
+      |> where([snap], snap.snapshot_id < ^"#{camera_id}_#{end_timestamp}")
+      |> limit(1)
+      |> SnapshotRepo.one
+    kept_snapshots =
+      Snapshot
+      |> where([snap], snap.snapshot_id > ^"#{camera_id}_#{begin_timestamp}")
+      |> where([snap], snap.snapshot_id < ^"#{camera_id}_#{end_timestamp}")
+      |> where([snap], snap.notes != "Evercam Proxy")
+      |> SnapshotRepo.all
 
-    snapshots =
-      from(snap in Snapshot,
-        where: snap.snapshot_id > ^"#{camera_id}_#{begin_timestamp}",
-        where: snap.snapshot_id < ^"#{camera_id}_#{end_timestamp}",
-        where: snap.notes == "Evercam Proxy")
+    cond do
+      any_snapshot == nil ->
+        [[], []]
+      kept_snapshots == [] ->
+        [[begin_timestamp, end_timestamp]]
+      true ->
+        construct_ranges(camera_id, kept_snapshots, begin_timestamp, end_timestamp)
+    end
+  end
 
-    SnapshotRepo.all(snapshots)
+  defp construct_ranges(camera_id, snapshots, start, finish) do
+    snapshots = Enum.map(snapshots, fn(s) -> String.replace(s.snapshot_id, "#{camera_id}_", "") end)
+    []
+    |> Enum.into([finish])
+    |> Enum.into(snapshots)
+    |> Enum.into([start])
+    |> Enum.chunk(2, 1)
   end
 
   def changeset(snapshot, params \\ :empty) do
