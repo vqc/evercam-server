c3e904a2fa6cc77ba2bd856093f83a19aa4237bc
Miloš Mošić <mosic.milos@gmail.com>
2016-01-28 13:20:18 +0100

    Add AccessRight model

---
+++ web/models/access_right.ex
@@ -0,0 +1,18 @@
+defmodule AccessRight do
+  use EvercamMedia.Web, :model
+
+  schema "access_rights" do
+    belongs_to :access_token, AccessToken, foreign_key: :token_id
+    belongs_to :camera, Camera, foreign_key: :camera_id
+    belongs_to :grantor, User, foreign_key: :grantor_id
+    belongs_to :snapshot, Snapshot, foreign_key: :snapshot_id
+    belongs_to :account, User, foreign_key: :account_id
+
+    field :right, :string
+    field :status, :integer
+    field :scope, :string
+
+    field :updated_at, Ecto.DateTime, default: Ecto.DateTime.utc
+    field :created_at, Ecto.DateTime, default: Ecto.DateTime.utc
+  end
+end
