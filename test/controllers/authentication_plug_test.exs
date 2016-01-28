73c16669624f4bbc6dff762a39dae0093e163ce6
Miloš Mošić <mosic.milos@gmail.com>
2016-01-28 18:42:34 +0100

    Formatting

--- test/controllers/authentication_plug_test.exs
+++ test/controllers/authentication_plug_test.exs
@@ -1,5 +1,6 @@
 defmodule EvercamMedia.AuthenticationPlugTest do
   use EvercamMedia.PlugCase
+  alias EvercamMedia.Repo
 
   test "returns HTTP 401 with error message if api_id and api_key are invalid" do
     conn = conn(:put, "/v1/users/1")
@@ -11,12 +12,13 @@ defmodule EvercamMedia.AuthenticationPlugTest do
   end
 
   test "returns HTTP 200 if api_id and api_key query strings are valid" do
-    {:ok, country} = EvercamMedia.Repo.insert(%Country{name: "Whatever", iso3166_a2: "WHT"})
-    {:ok, user} = EvercamMedia.Repo.insert(%User{ firstname: "Paul", lastname: "McCartney", 
-        username: "paulm", password: "whatever", email: "paul@mccartney.com", 
-        api_id: "some_api_id", api_key: "some_api_key", country_id: country.id})
+    country = Repo.insert!(%Country{name: "Whatever", iso3166_a2: "WHT"})
+    user = Repo.insert!(%User{firstname: "Paul", lastname: "McCartney",
+      username: "paulm", password: "whatever", email: "paul@mccartney.com",
+      api_id: "some_api_id", api_key: "some_api_key", country_id: country.id})
 
-    conn = conn(:put, "/v1/users/#{user.id}?api_id=#{user.api_id}&api_key=#{user.api_key}", %{ "user" => %{ firstname: "whatever" }} )
+    conn =
+      conn(:put, "/v1/users/#{user.id}?api_id=#{user.api_id}&api_key=#{user.api_key}", %{ "user" => %{ firstname: "whatever" }} )
       |> EvercamMedia.Router.call(@opts)
 
     assert conn.state == :sent
@@ -24,12 +26,13 @@ defmodule EvercamMedia.AuthenticationPlugTest do
   end
 
   test "returns HTTP 200 if x-api-key and x-api-id headers are valid" do
-    {:ok, country} = EvercamMedia.Repo.insert(%Country{name: "Whatever", iso3166_a2: "WHT"})
-    {:ok, user} = EvercamMedia.Repo.insert(%User{ firstname: "Paul", lastname: "McCartney", 
-        username: "paulm", password: "whatever", email: "paul@mccartney.com", 
-        api_id: "some_api_id", api_key: "some_api_key", country_id: country.id})
+    country = Repo.insert!(%Country{name: "Whatever", iso3166_a2: "WHT"})
+    user = Repo.insert!(%User{firstname: "Paul", lastname: "McCartney",
+      username: "paulm", password: "whatever", email: "paul@mccartney.com",
+      api_id: "some_api_id", api_key: "some_api_key", country_id: country.id})
 
-    conn = conn(:put, "/v1/users/#{user.id}", %{ "user" => %{ firstname: "whatever" }} )
+    conn =
+      conn(:put, "/v1/users/#{user.id}", %{ "user" => %{ firstname: "whatever" }} )
       |> Plug.Conn.put_req_header("x-api-key", user.api_key)
       |> Plug.Conn.put_req_header("x-api-id", user.api_id)
       |> EvercamMedia.Router.call(@opts)
--- test/evercam_media/auth_test.exs
+++ test/evercam_media/auth_test.exs
@@ -2,12 +2,12 @@ defmodule EvercamMedia.AuthTest do
   use EvercamMedia.ModelCase
 
   setup do
-    {:ok, country } = Repo.insert(%Country{name: "Whatever", iso3166_a2: "WHTEVR"})
-    {:ok, user} = Repo.insert(%User{firstname: "Jake", lastname: "Doe",
+    country = Repo.insert!(%Country{name: "Whatever", iso3166_a2: "WHTEVR"})
+    user = Repo.insert!(%User{firstname: "Jake", lastname: "Doe",
       email: "jake@doe.com", api_id: UUID.uuid4(:hex), api_key: UUID.uuid4(:hex),
       username: "jake_doe", password: "whatever123", country_id: country.id})
 
-    { :ok, country: country, user: user }
+    {:ok, country: country, user: user}
   end
 
   test "returns :valid & user if api_id and api_key are valid", context do
