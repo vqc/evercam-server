c9f2f41fd6418f73553e8212182ef50a122c556f
Miloš Mošić <mosic.milos@gmail.com>
2016-01-28 23:02:23 +0100

    Fix test warnings

--- test/permissions/camera_test.exs
+++ test/permissions/camera_test.exs
@@ -11,7 +11,7 @@ defmodule Permissions.CameraTest do
     user = Repo.insert!(%User{firstname: "John", lastname: "Doe", username: "johndoe", email: "john@doe.com", password: "password123", country_id: country.id})
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: user.id, name: "camera 1", exid: "123", is_public: false, config: ""})
-    access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
+    _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
 
     assert Permissions.Camera.can_edit?(user, camera.exid)
     assert Permissions.Camera.can_view?(user, camera.exid)
@@ -27,7 +27,7 @@ defmodule Permissions.CameraTest do
     owner = Repo.insert!(%User{firstname: "Jane", lastname: "Doe", username: "janedoe", email: "jane@doe.net", password: "password123", country_id: country.id})
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
-    access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "view", camera_id: camera.id, status: 1, scope: "cameras"})
+    _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "view", camera_id: camera.id, status: 1, scope: "cameras"})
 
     assert Permissions.Camera.can_view?(user, camera.exid)
     refute Permissions.Camera.can_edit?(user, camera.exid)
@@ -39,7 +39,7 @@ defmodule Permissions.CameraTest do
     owner = Repo.insert!(%User{firstname: "Jane", lastname: "Doe", username: "janedoe", email: "jane@doe.net", password: "password123", country_id: country.id})
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
-    access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
+    _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
 
     assert Permissions.Camera.can_edit?(user, camera.exid)
   end
@@ -50,8 +50,8 @@ defmodule Permissions.CameraTest do
     owner = Repo.insert!(%User{firstname: "Jane", lastname: "Doe", username: "janedoe", email: "jane@doe.net", password: "password123", country_id: country.id})
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
-    grant_right = Repo.insert!(%AccessRight{token_id: token.id, right: "grant", camera_id: camera.id, status: 1, scope: "cameras"})
-    view_right = Repo.insert!(%AccessRight{token_id: token.id, right: "list", camera_id: camera.id, status: 1, scope: "cameras"})
+    _grant_right = Repo.insert!(%AccessRight{token_id: token.id, right: "grant", camera_id: camera.id, status: 1, scope: "cameras"})
+    _view_right = Repo.insert!(%AccessRight{token_id: token.id, right: "list", camera_id: camera.id, status: 1, scope: "cameras"})
 
     assert Permissions.Camera.can_list?(user, camera.exid)
     assert Permissions.Camera.can_grant?(user, camera.exid)
