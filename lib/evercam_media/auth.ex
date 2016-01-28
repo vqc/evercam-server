b61639392ae8adeaca0614e9f42272346de187d3
Miloš Mošić <mosic.milos@gmail.com>
2016-01-28 13:33:26 +0100

    Update auth to work with new User model

--- lib/evercam_media/auth.ex
+++ lib/evercam_media/auth.ex
@@ -1,7 +1,7 @@
 defmodule EvercamMedia.Auth do
   def validate(api_id, api_key) do
     if present?(api_id) && present?(api_key) do
-      case EvercamMedia.Repo.one(User.find_by_api_keys(api_id, api_key)) do
+      case User.find_by_api_keys(api_id, api_key) do
         %User{} ->
           :valid
         nil ->
