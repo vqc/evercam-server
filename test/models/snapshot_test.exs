4fed6790f5d5baba6854514c4fefcf39f2a79446
Miloš Mošić <mosic.milos@gmail.com>
2016-01-28 14:09:59 +0100

    Fix snapshot test by making the timestamps relative

--- test/models/snapshot_test.exs
+++ test/models/snapshot_test.exs
@@ -1,8 +1,17 @@
 defmodule SnapshotTest do
   use EvercamMedia.ModelCase
   alias EvercamMedia.Util
-
+  alias Calendar.Date
+  alias Calendar.DateTime
+  alias Calendar.Strftime
+  alias Calendar.NaiveDateTime
   setup do
+    seconds_to_8_days_ago = (8) * (24 * 60 * 60) * (-1)
+    expiry_day =
+      DateTime.now_utc
+      |> DateTime.advance!(seconds_to_8_days_ago)
+      |> Strftime.strftime!("%Y%m%d")
+
     country = Repo.insert!(
       %Country{
         name: "Aruba",
@@ -39,24 +48,26 @@ defmodule SnapshotTest do
     SnapshotRepo.insert!(
       %Snapshot{
         camera_id: camera.id,
-        snapshot_id: Util.format_snapshot_id(camera.id, "2016011902242662241"),
+        snapshot_id: Util.format_snapshot_id(camera.id, "#{expiry_day}02242662241"),
         notes: "Evercam Proxy"})
     SnapshotRepo.insert!(
       %Snapshot{
         camera_id: camera.id,
-        snapshot_id: Util.format_snapshot_id(camera.id, "2016011903310662241"),
+        snapshot_id: Util.format_snapshot_id(camera.id, "#{expiry_day}03310662241"),
         notes: "Evercam Proxy"})
     SnapshotRepo.insert!(
       %Snapshot{
         camera_id: camera.id,
-        snapshot_id: Util.format_snapshot_id(camera.id, "2016011903474662241"),
+        snapshot_id: Util.format_snapshot_id(camera.id, "#{expiry_day}03474662241"),
         notes: "Custom Note"})
 
-    {:ok, cloud_recording: Repo.preload(cloud_recording, :camera)}
+    {:ok, cloud_recording: Repo.preload(cloud_recording, :camera), expiry_day: expiry_day}
   end
 
-  test "lists ranges of expired snapshots", %{cloud_recording: cloud_recording} do
-    ranges = [["20160119000000", "20160119034746622"], ["20160119034746622", "20160120000000"]]
+  test "lists ranges of expired snapshots", %{cloud_recording: cloud_recording, expiry_day: expiry_day} do
+    {:ok, timestamp, _} = NaiveDateTime.Parse.asn1_generalized("#{expiry_day}000000")
+    end_day = timestamp |> Date.next_day! |> Strftime.strftime!("%Y%m%d")
+    ranges = [["#{expiry_day}000000", "#{expiry_day}034746622"], ["#{expiry_day}034746622", "#{end_day}000000"]]
     assert Snapshot.expired(cloud_recording) == ranges
   end
 end
