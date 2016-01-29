a2e568fd26ba68e6cb46305743d0d8587623592e
Miloš Mošić <mosic.milos@gmail.com>
2016-01-29 18:15:12 +0100

    Support nil credentials in auth

--- lib/evercam_media/auth.ex
+++ lib/evercam_media/auth.ex
@@ -1,7 +1,7 @@
 defmodule EvercamMedia.Auth do
   def validate("", ""), do: :valid
   def validate(api_id, api_key) do
-    case User.get_by_api_keys(api_id, api_key) do
+    case User.get_by_api_keys(to_string(api_id), to_string(api_key)) do
       nil ->
         :invalid
       user ->
