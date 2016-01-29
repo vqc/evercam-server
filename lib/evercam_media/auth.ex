76a62b6d220421a747fae2f8469ebc1e2de477a5
Miloš Mošić <mosic.milos@gmail.com>
2016-01-29 16:33:34 +0100

    Simplify Auth module

--- lib/evercam_media/auth.ex
+++ lib/evercam_media/auth.ex
@@ -1,18 +1,11 @@
 defmodule EvercamMedia.Auth do
+  def validate("", ""), do: :valid
   def validate(api_id, api_key) do
-    if present?(api_id) && present?(api_key) do
-      case User.get_by_api_keys(api_id, api_key) do
-        nil ->
-          :invalid
-        user ->
-          {:valid, user}
-      end
-    else
-      :valid
+    case User.get_by_api_keys(api_id, api_key) do
+      nil ->
+        :invalid
+      user ->
+        {:valid, user}
     end
-  end
-
-  defp present?(str) do
-    is_binary(str) && str != ""
   end
 end
