a92deddbb4a00e18a71ab9bd7cca748a9d2947ef
Miloš Mošić <mosic.milos@gmail.com>
2016-02-06 05:28:36 +0100

    Enable storing snapshots on the FTP server

--- lib/evercam_media/snapshot/s3_upload_handler.ex
+++ lib/evercam_media/snapshot/s3_upload_handler.ex
@@ -5,12 +5,16 @@ defmodule EvercamMedia.Snapshot.S3UploadHandler do
 
   use GenEvent
   alias EvercamMedia.Snapshot.S3
+  alias EvercamMedia.Snapshot.Storage
   require Logger
 
   def handle_event({:got_snapshot, data}, state) do
     {camera_exid, timestamp, image} = data
     spawn fn ->
       S3.upload(camera_exid, timestamp, image)
+    end
+    spawn fn ->
+      Storage.upload(camera_exid, timestamp, image)
     end
     {:ok, state}
   end
