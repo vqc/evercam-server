b4497c457a975f9256d66ad07af466136d3f5770
Miloš Mošić <mosic.milos@gmail.com>
2016-02-08 16:38:27 +0100

    Update auth tests

--- test/evercam_media/auth_test.exs
+++ test/evercam_media/auth_test.exs
@@ -11,14 +11,14 @@ defmodule EvercamMedia.AuthTest do
   end
 
   test "returns :valid & user if api_id and api_key are valid", context do
-    assert EvercamMedia.Auth.validate(context[:user].api_id, context[:user].api_key) == {:valid, context[:user]}
+    assert EvercamMedia.Auth.validate(context[:user].api_id, context[:user].api_key, "") == {:valid, context[:user]}
   end
 
   test "returns :invalid if api_id and api_key are invalid" do
-    assert EvercamMedia.Auth.validate("some_invalid_api_id", "some_invalid_api_key") == :invalid
+    assert EvercamMedia.Auth.validate("some_invalid_api_id", "some_invalid_api_key", "") == :invalid
   end
 
   test "returns :valid if api_id and api_key are missing" do
-    assert EvercamMedia.Auth.validate("", "") == :valid
+    assert EvercamMedia.Auth.validate("", "", "") == :valid
   end
 end
