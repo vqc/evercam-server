3a9f3615b135190e6eabaf5e23e79a432b3da8c8
Miloš Mošić <mosic.milos@gmail.com>
2016-04-04 18:48:03 +0200

    Use a variable for the current year in emails

--- lib/evercam_media/user_mailer.ex
+++ lib/evercam_media/user_mailer.ex
@@ -1,14 +1,17 @@
 defmodule EvercamMedia.UserMailer do
+  use Calendar
   alias EvercamMedia.Snapshot.Storage
+
   @config Application.get_env(:evercam_media, :mailgun)
   @from Application.get_env(:evercam_media, EvercamMedia.Endpoint)[:email]
+  @year DateTime.now_utc |> Strftime.strftime!("%Y")
 
   def confirm(user, code) do
     Mailgun.Client.send_email @config,
       to: user.email,
       subject: "Evercam Confirmation",
       from: @from,
-      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "confirm.html", user: user, code: code),
+      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "confirm.html", user: user, code: code, year: @year),
       text: Phoenix.View.render_to_string(EvercamMedia.EmailView, "confirm.txt", user: user, code: code)
   end
 
@@ -19,7 +22,7 @@ defmodule EvercamMedia.UserMailer do
       subject: "Evercam Camera \"#{camera.name}\" is now #{status}",
       from: @from,
       attachments: attachments(thumbnail),
-      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.html", user: user, camera: camera, thumbnail_available: !!thumbnail),
+      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.html", user: user, camera: camera, thumbnail_available: !!thumbnail, year: @year),
       text: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.txt", user: user, camera: camera)
   end
 
