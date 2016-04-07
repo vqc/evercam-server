b7ca7bc6ceed705a7d3dd1a1307444e9d2ceb722
Miloš Mošić <mosic.milos@gmail.com>
2016-04-07 14:55:17 +0200

    Update camera permissions tests

--- test/permissions/camera_test.exs
+++ test/permissions/camera_test.exs
@@ -12,13 +12,14 @@ defmodule Permissions.CameraTest do
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: user.id, name: "camera 1", exid: "123", is_public: false, config: ""})
     _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
+    camera = Repo.preload(camera, :access_rights)
 
-    assert Permissions.Camera.can_edit?(user, camera.exid)
-    assert Permissions.Camera.can_view?(user, camera.exid)
-    assert Permissions.Camera.can_snapshot?(user, camera.exid)
-    assert Permissions.Camera.can_delete?(user, camera.exid)
-    assert Permissions.Camera.can_list?(user, camera.exid)
-    assert Permissions.Camera.can_grant?(user, camera.exid)
+    assert Permissions.Camera.can_edit?(user, camera)
+    assert Permissions.Camera.can_view?(user, camera)
+    assert Permissions.Camera.can_snapshot?(user, camera)
+    assert Permissions.Camera.can_delete?(user, camera)
+    assert Permissions.Camera.can_list?(user, camera)
+    assert Permissions.Camera.can_grant?(user, camera)
   end
 
   test ".can_edit(user, camera) - returns false if the USER cannot EDIT the CAMERA" do
@@ -28,9 +29,10 @@ defmodule Permissions.CameraTest do
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
     _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "view", camera_id: camera.id, status: 1, scope: "cameras"})
+    camera = Repo.preload(camera, :access_rights)
 
-    assert Permissions.Camera.can_view?(user, camera.exid)
-    refute Permissions.Camera.can_edit?(user, camera.exid)
+    assert Permissions.Camera.can_view?(user, camera)
+    refute Permissions.Camera.can_edit?(user, camera)
   end
 
   test ".can_edit(user, camera) - returns true if the USER can EDIT the CAMERA" do
@@ -40,8 +42,9 @@ defmodule Permissions.CameraTest do
     token = Repo.insert!(%AccessToken{user_id: user.id, is_revoked: false, request: "whatever", expires_at: Ecto.DateTime.from_erl({{2020,01,01}, {10,10,10}})})
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
     _access_right = Repo.insert!(%AccessRight{token_id: token.id, right: "edit", camera_id: camera.id, status: 1, scope: "cameras"})
+    camera = Repo.preload(camera, :access_rights)
 
-    assert Permissions.Camera.can_edit?(user, camera.exid)
+    assert Permissions.Camera.can_edit?(user, camera)
   end
 
   test ".can_edit(user, camera) - returns false if USER has other access rights but not EDIT" do
@@ -52,9 +55,10 @@ defmodule Permissions.CameraTest do
     camera = Repo.insert!(%Camera{owner_id: owner.id, name: "camera 1", exid: "123", is_public: false, config: ""})
     _grant_right = Repo.insert!(%AccessRight{token_id: token.id, right: "grant", camera_id: camera.id, status: 1, scope: "cameras"})
     _view_right = Repo.insert!(%AccessRight{token_id: token.id, right: "list", camera_id: camera.id, status: 1, scope: "cameras"})
+    camera = Repo.preload(camera, :access_rights)
 
-    assert Permissions.Camera.can_list?(user, camera.exid)
-    assert Permissions.Camera.can_grant?(user, camera.exid)
-    refute Permissions.Camera.can_edit?(user, camera.exid)
+    assert Permissions.Camera.can_list?(user, camera)
+    assert Permissions.Camera.can_grant?(user, camera)
+    refute Permissions.Camera.can_edit?(user, camera)
   end
 end
