0c14928aa5011f4fde6848168666baab00430349
Miloš Mošić <mosic.milos@gmail.com>
2016-05-27 12:51:10 +0200

    Remove forced timeout from live/snapshot endpoint

--- web/controllers/snapshot_controller.ex
+++ web/controllers/snapshot_controller.ex
@@ -10,8 +10,7 @@ defmodule EvercamMedia.SnapshotController do
   @optional_params %{"notes" => nil, "with_data" => false}
 
   def show(conn, %{"id" => camera_exid}) do
-    function = fn -> snapshot_with_user(camera_exid, conn.assigns[:current_user], false) end
-    [code, response] = Util.exec_with_timeout(function, 15)
+    [code, response] = snapshot_with_user(camera_exid, conn.assigns[:current_user], false)
     show_render(conn, code, response)
   end
 
