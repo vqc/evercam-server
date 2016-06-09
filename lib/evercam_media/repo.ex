0823a5b5ab8e5d783ba80848024f957fa28c0460
Miloš Mošić <mosic.milos@gmail.com>
2016-06-09 20:10:30 +0200

    Add exists?/1 function to Repo

--- lib/evercam_media/repo.ex
+++ lib/evercam_media/repo.ex
@@ -4,4 +4,16 @@ end
 
 defmodule EvercamMedia.SnapshotRepo do
   use Ecto.Repo, otp_app: :evercam_media
+  require Ecto.Query
+
+  def exists?(queryable) do
+    queryable
+    |> Ecto.Query.from(select: 1, limit: 1)
+    |> Ecto.Queryable.to_query
+    |> EvercamMedia.SnapshotRepo.one
+    |> case do
+      1 -> true
+      _ -> false
+    end
+  end
 end
