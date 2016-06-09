0ef2ab7785cecf052fb69783d3512cadf17d1ba6
Miloš Mošić <mosic.milos@gmail.com>
2016-06-09 22:51:20 +0200

    Deleted unused view

--- web/views/changeset_view.ex
+++
@@ -1,7 +0,0 @@
-defmodule EvercamMedia.ChangesetView do
-  use EvercamMedia.Web, :view
-
-  def render("error.json", %{changeset: changeset}) do
-    %{errors: Enum.into(changeset.errors, %{})}
-  end
-end
