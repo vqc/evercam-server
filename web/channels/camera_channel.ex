dc14539d2b8027bd16721c660633069652796f1b
Miloš Mošić <mosic.milos@gmail.com>
2016-07-21 23:50:26 +0200

    Add authorization to camera channels

--- web/channels/camera_channel.ex
+++ web/channels/camera_channel.ex
@@ -1,10 +1,18 @@
 defmodule EvercamMedia.CameraChannel do
   use Phoenix.Channel
   alias EvercamMedia.Snapshot.StreamerSupervisor
+  alias EvercamMedia.Util
 
   def join("cameras:" <> camera_exid, _auth_msg, socket) do
-    send(self, {:after_join, camera_exid})
-    {:ok, socket}
+    camera = Camera.get_full(camera_exid)
+    user = Util.deep_get(socket, [:assigns, :current_user], nil)
+
+    if Permission.Camera.can_snapshot?(user, camera) do
+      send(self, {:after_join, camera_exid})
+      {:ok, socket}
+    else
+      {:error, "Unauthorized."}
+    end
   end
 
   def handle_info({:after_join, camera_exid}, socket) do
