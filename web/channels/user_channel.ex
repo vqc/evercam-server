091466935cc31a1a91c96443b8324842e26d0a60
Miloš Mošić <mosic.milos@gmail.com>
2016-07-21 23:55:42 +0200

    Add authorization to user channels

--- web/channels/user_channel.ex
+++ web/channels/user_channel.ex
@@ -1,13 +1,19 @@
 defmodule EvercamMedia.UserChannel do
   use Phoenix.Channel
-  alias EvercamMedia.Auth
+  alias EvercamMedia.Util
 
-  def join("users:" <> username, params, socket) do
-    send(self, {:after_join, username})
-    {:ok, socket}
+  def join("users:" <> username, _params, socket) do
+    caller_username = Util.deep_get(socket, [:assigns, :current_user, :username], "")
+
+    if username == caller_username do
+      send(self, {:after_join, username})
+      {:ok, socket}
+    else
+      {:error, "Unauthorized."}
+    end
   end
 
-  def handle_info({:after_join, username}, socket) do
+  def handle_info({:after_join, _username}, socket) do
     {:noreply, socket}
   end
 end
