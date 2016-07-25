0c0ea5da6dbc21c7727181f21c171503e6b7509b
Miloš Mošić <mosic.milos@gmail.com>
2016-07-25 15:58:48 +0200

    Fix password hashing

--- lib/mix/tasks/hash_password.ex
+++ lib/mix/tasks/hash_password.ex
@@ -10,11 +10,14 @@ defmodule EvercamMedia.HashPassword do
     |> where([user], fragment("LENGTH(?) < 50", user.password))
     |> Repo.all
     |> Enum.each(fn(user) ->
-      changeset = User.changeset(user, %{password: user.password})
-
-      case Repo.update(changeset) do
-        {:ok, user} ->
-          Logger.info "Password successfully updated for user '#{user.username}'"
+      password = user.password
+      with changeset = User.changeset(user, %{password: "1234567890"}),
+           {:ok, user} <- Repo.update(changeset),
+           changeset = User.changeset(user, %{password: password}),
+           {:ok, user} <- Repo.update(changeset)
+      do
+        Logger.info "Password successfully updated for user '#{user.username}'"
+      else
         {:error, _changeset} ->
           Logger.error "Error updating password for user '#{user.username}'"
       end
