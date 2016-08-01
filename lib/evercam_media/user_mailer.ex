d87a544a28dd7e1ece3ff337d4bf28a0432136ae
Junaid Farooq <junaid@evercam.io>
2016-08-01 14:14:10 +0500

    corrected the text of email and also added time on which last image h… (#357)
    * corrected the text of email and also added time on which last image has been taken
    
    * Changed preposition

--- lib/evercam_media/user_mailer.ex
+++ lib/evercam_media/user_mailer.ex
@@ -15,13 +15,15 @@ defmodule EvercamMedia.UserMailer do
   end
 
   def camera_status(status, user, camera) do
+    timezone = camera |> Camera.get_timezone
+    current_time = Calendar.DateTime.now_utc |> Calendar.DateTime.shift_zone!(timezone) |> Calendar.Strftime.strftime!("%A, %d %b %Y %l:%M %p")
     thumbnail = nil
     Mailgun.Client.send_email @config,
       to: user.email,
-      subject: "Evercam Camera \"#{camera.name}\" is now #{status}",
+      subject: "\"#{camera.name}\" camera is now #{status}",
       from: @from,
       attachments: get_attachments(thumbnail),
-      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.html", user: user, camera: camera, thumbnail_available: !!thumbnail, year: @year),
+      html: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.html", user: user, camera: camera, thumbnail_available: !!thumbnail, year: @year, current_time: current_time),
       text: Phoenix.View.render_to_string(EvercamMedia.EmailView, "#{status}.txt", user: user, camera: camera)
   end
 
