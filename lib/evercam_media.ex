d63a6b6eb7733282e94045d57793e5757b5c425f
Junaid Farooq <junaid@evercam.io>
2017-03-15 09:35:51 +0500

    Reduced max connection and timeout for seaweedfs hackney_pool.

--- lib/evercam_media.ex
+++ lib/evercam_media.ex
@@ -24,8 +24,8 @@ defmodule EvercamMedia do
       supervisor(EvercamMedia.Snapshot.WorkerSupervisor, []),
       supervisor(EvercamMedia.Snapmail.SnapmailerSupervisor, []),
       :hackney_pool.child_spec(:snapshot_pool, [timeout: 5000, max_connections: 1000]),
-      :hackney_pool.child_spec(:seaweedfs_upload_pool, [timeout: 10_000, max_connections: 5000]),
-      :hackney_pool.child_spec(:seaweedfs_download_pool, [timeout: 10_000, max_connections: 5000]),
+      :hackney_pool.child_spec(:seaweedfs_upload_pool, [timeout: 5000, max_connections: 1000]),
+      :hackney_pool.child_spec(:seaweedfs_download_pool, [timeout: 5000, max_connections: 1000]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
